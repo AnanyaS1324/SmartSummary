702eb198bdf05171489901c3d332ded6f2c8ac03
AnanyaS1324 <shriana1324@gmail.com>
2025-01-15 22:39:05 +0530

    Update tailwindcss-config.js

--- tailwindcss-config.js
+++ tailwindcss-config.js
@@ -26,7 +26,7 @@ module.exports = {
       emerald: colors.emerald,
       teal: colors.teal,
       cyan: colors.cyan,
-      lightBlue: colors.lightBlue,
+      sky: colors.sky,
       violet: colors.violet,
       fuchsia: colors.fuchsia,
       rose: colors.rose,
